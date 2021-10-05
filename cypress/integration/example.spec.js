8250c9f458c3443a3998e4074f4a11cff207a625
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-05 15:25:58 -0700

    completed html cypress testing

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,12 @@
 
-describe('Example Test', ()=>{
+describe('html layout', ()=>{
     before(()=>{
         cy.visit('/');
     });
 
-    describe('HTML elements', ()=>{   
-        it('contains the alchemy logo', ()=>{
-            cy.get('.main-logo').should('be.visible');
+    describe('html layout', ()=>{   
+        it('h1 should appear in html file', ()=>{
+            cy.get('h1').should('be.visible');
         });
     });
 });
